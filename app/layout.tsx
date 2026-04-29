8f2a6281afade837fc2b0308442bd678ddce8373
AsifMohamed <70835051+AsifMohamed@users.noreply.github.com>
2026-04-29 13:57:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from 'next'
 import { Poppins } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-pnpm approve-builds 
 
 const poppins = Poppins({ 
   subsets: ["latin"],
